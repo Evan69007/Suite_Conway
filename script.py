349e2348a3ff58a81d944ebb3f04c07c5dc73c57
Evan <evanaguttesrivelon@gmail.com>
2025-05-28 14:04:44 +0200

    suite de conway finie

---
+++ script.py
@@ -0,0 +1,40 @@
+def decoupeChaine(string):
+	char = string[0]
+	resultStr = "" + char
+	
+	for i in range(1, len(string)):
+		if string[i] != char:
+			resultStr += " "
+			char = string[i]
+		resultStr += string[i]
+	
+	return (resultStr)
+
+
+# print(decoupeChaine("aabbbca"))
+
+def decritChaine(string):
+	resultStr = ""
+	i = 0
+	while i < len(string):
+		char = string[i]
+		nb_char = 1
+		i += 1
+		while(i < len(string) and string[i] == char):
+			nb_char += 1
+			i += 1
+		resultStr += (str(nb_char) + char)
+	return (resultStr)
+
+# print(decritChaine("aaabbc"))
+
+
+def suiteConway(char, n):
+	string = char
+	i = 0
+	while i < n:
+		print(string)
+		string = decritChaine(string)
+		i += 1
+
+suiteConway('a', 7)
